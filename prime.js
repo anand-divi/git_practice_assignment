72d6cd81ec326555e962bb594e79ec8f336b9392
anand-divi <akhil244978@gmail.com>
2022-11-02 12:09:30 +0530

    Created prime code

---
+++ prime.js
@@ -0,0 +1,30 @@
+
+
+function check_prime(num){
+
+var cnt=0;
+
+for(var i=1;i<=num;i++){
+if(num%i==0)
+{
+cnt++;
+}
+}
+
+if(cnt==2){
+return true;
+}
+else
+return false;
+
+}
+
+var ans=check_prime(13);
+
+if(ans==false)
+console.log("prime");
+else
+console.log("not prime");
+
+
+
